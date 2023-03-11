7827cfdef698165f55d5fb763eb6cf248d2edfae
Prince Sky Sithole <117398461+Princeskydon8@users.noreply.github.com>
2023-03-11 13:22:49 +0200

    sensational update

--- script.js
+++ script.js
@@ -1,47 +1,63 @@
 let playerScore = 0;
 let computerScore = 0;
-let playerName = prompt('Enter Player name')
-game();
+
+// Prompt for player name with input validation
+let playerName = '';
+while (playerName.length===0) {
+  playerName = prompt('Enter Player name');
+}
 
 // computer select function
-
 function computerPlay() {
-    const option = ['rock', 'paper', 'scissors'];
-    let random = Math.floor(Math.random() * option.length);
-    return option[random];
+  const options = ['rock', 'paper', 'scissors'];
+  let random = Math.floor(Math.random() * options.length);
+  return options[random];
 }
 
-// player select function
+// player select function with input validation
 function playerPlay() {
-    
-    let correctInput =  true;
-    // Loop will run until the user gives a correct input
-    while(correctInput)
-    {
-        const input = prompt('Rock,Paper,Scissors?');
-        const option = input.toLowerCase();
+  const maxAttempts = 5;
+  let attempts = 0;
 
-        if(option === 'rock' || option === 'paper' || option === 'scissors')
-        {  
-            return option;
-        }
-        else
-        {
-            alert('Incorrect input! Try again');
-        }
+  while (attempts < maxAttempts) {
+    const input = prompt('Rock, Paper, Scissors?');
+    const option = input.toLowerCase();
+    
+   
+    if (option === 'rock' || option === 'paper' || option === 'scissors') {
+      return option;
+    } else {
+      alert('Incorrect input! Try again');
+      attempts++;
     }
+  }
+
+  // Maximum number of attempts exceeded
+  alert(`You have exceeded the maximum number of attempts (${maxAttempts}). The game will now exit.`);
+  throw new Error('Maximum number of attempts exceeded');
 }
+
+// automatic play function
+//function autoPlay() {
+  //  const playerSelection = playerPlay();
+    //const computerSelection = computerPlay();
+   // alert(playRound(playerSelection, computerSelection));
+//}    
+
+//cheat function
+//function cheat() {
+  //  playerScore++;
+    //alert(`${playerName} of the PeakyBlinders is Inevitable!!`);   
+//}
 
 // play 1 single round
 function playRound(playerSelection, computerSelection) {
-
-    if(playerSelection === computerSelection) {
+     if(playerSelection === computerSelection) {
         return 'It is a tie';
     }
 
 
     if(playerSelection === 'rock') {
-
         if(computerSelection === 'scissors') {
             playerScore++;
             return `${playerName} wins with rock`;
@@ -52,7 +68,6 @@ function playRound(playerSelection, computerSelection) {
     }
 
     if(playerSelection === 'paper') {
-
         if(computerSelection === 'rock') {
             playerScore++;
             return `${playerName} wins with paper`;
@@ -63,8 +78,7 @@ function playRound(playerSelection, computerSelection) {
     }
 
     if(playerSelection === 'scissors') {
-
-        if(computerSelection === "paper") {
+         if(computerSelection === "paper") {
             playerScore++;
             return `${playerName} wins with scissors`;
         } else if(computerSelection === 'rock') {
@@ -72,14 +86,17 @@ function playRound(playerSelection, computerSelection) {
             return 'Branko wins with rock';
         }
     }
-
 }
 
+// reset function
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
+}    
 
 // game
 function game() {
-
-    for(var i = 1; i <= 5; i++) {
+     for(var i = 1; i <= 5; i++) {
        const playerSelection = playerPlay();
        const computerSelection = computerPlay();
        alert(playRound(playerSelection, computerSelection));
@@ -90,15 +107,19 @@ function game() {
 
     if(playerScore>computerScore){
         confirm(`Match result: ${playerName} Wins: ${playerScore} - ${computerScore} . Press Ok to restart.`)
-            window.location.reload("./");
+        reset();
+        game();
     }
     else if(playerScore<computerScore){
         confirm(`Match result: Branko Wins: ${computerScore} - ${playerScore} . Press Ok to restart`)
-            window.location.reload("./");
+        reset();
+        game();
     }
     else{
         confirm(`Match result: It is a tie! ${playerScore} - ${computerScore} . Press Ok to restart`)
-            window.location.reload("./");
+        reset();
+        game();
     }
-
 }
+// start the game
+game();
