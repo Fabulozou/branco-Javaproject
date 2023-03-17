e9154bfca996a98f861f31652e14060b3f1afeeb
LittleCanOpener <hrafn8@gmail.com>
2023-03-17 12:12:53 +0000

    removed alot of duplicates

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 let playerScore = 0;
 let computerScore = 0;
 
-
+// computer select function
+function computerPlay() {
+    const option = ['rock', 'paper', 'scissors'];
+    let random = Math.floor(Math.random() * option.length);
+    return option[random];
+}
+// player select function
 // Prompt for player name with input validation
 let playerName = prompt('Welcome ! Enter Player name')
 game();
 while (playerName.trim() === '') {
   playerName = prompt('Enter Player name');
 }
-
-// computer select function
-
-
-function computerPlay() {
-    const option = ['rock', 'paper', 'scissors'];
-    let random = Math.floor(Math.random() * option.length);
-    return option[random];
-}
-
 // player select function
 function playerPlay() {
   let correctInput = true;
@@ -33,189 +29,63 @@ function playerPlay() {
     }
   }
 }
-// play 1 single round with nested if statements
+// play round with nested if statements
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return 'It is a tie';
+    return `It is a tie current score: ${playerName} ${playerScore} - Branko ${computerScore}`;
   }
 
   if (playerSelection === 'rock') {
     if (computerSelection === 'scissors') {
       playerScore++;
-      return `${playerName} wins with rock`;
+      return `${playerName} Wins with Rock, score: ${playerName} ${playerScore} - Branko ${computerScore}`;
     } else if (computerSelection === 'paper') {
       computerScore++;
-      return 'Branko wins with paper';
+      return `Branko wins with Paper, score: Branko ${computerScore} - ${playerName} ${playerScore}`;
     }
   }
   if (playerSelection === 'paper') {
     if (computerSelection === 'rock') {
       playerScore++;
-      return `${playerName} wins with paper`;
+      return `${playerName} Wins with Paper, score ${playerScore} - Branko ${computerScore}`;
     } else if (computerSelection === 'scissors') {
       computerScore++;
-      return 'Branko wins with scissors';
-
-
-// player select function
-function playerPlay() {
-    
-    let correctInput =  true;
-    // Loop will run until the user gives a correct input
-    while(correctInput)
-    {
-        const input = prompt('Rock,Paper,Scissors?');
-        const option = input.toLowerCase();
-
-        if(option === 'rock' || option === 'paper' || option === 'scissors')
-        {  
-            return option;
-        }
-        else
-        {
-            alert('Incorrect input! Try again');
-        }
-    }
-}
-
-// play 1 single round
-function playRound(playerSelection, computerSelection) {
-
-    if(playerSelection === computerSelection) {
-        return 'It is a tie';
-    }
-
-
-    if(playerSelection === 'rock') {
-
-        if(computerSelection === 'scissors') {
-            playerScore++;
-            return `${playerName} wins with rock`;
-        } else if(computerSelection === 'paper') {
-            computerScore++;
-            return 'Branko wins with paper'
-        }
-    }
-
-    if(playerSelection === 'paper') {
-
-        if(computerSelection === 'rock') {
-            playerScore++;
-            return `${playerName} wins with paper`;
-        } else if(computerSelection === 'scissors') {
-            computerScore++;
-            return 'Branko wins with scissors';
-        }
-
+      return `Branko wins with Scissors, score: Branko ${computerScore} - ${playerName} ${playerScore}`;
     }
   }
-
-
   if (playerSelection === 'scissors') {
     if (computerSelection === 'paper') {
       playerScore++;
-      return `${playerName} wins with scissors`;
+      return `${playerName} Wins with Scissors, score: ${playerName} ${playerScore} - Branko ${computerScore}`;
     } else if (computerSelection === 'rock') {
       computerScore++;
-      return 'Branko wins with rock';
+      return `Branko wins with Rock, score: Branko ${computerScore} - ${playerName} ${playerScore}`;
     }
   }
 }
 
 // woza game functionality with no var 
 function game() {
-  let playCount = 0;
-  let playAgain = true;
-
-  while (playAgain) {
-    // Ask the user if they want to quit when cancel is pressed
-    let playerSelection = null;
-    do {
-      const input = prompt('Rock, Paper, Scissors? Type cancel to quit the game.');
-      if (input === null) {
-        if (confirm('Are you sure you want to quit the game?')) {
-          playAgain = false;
-          break;
-        }
-      } else if (input.trim().toLowerCase() === 'cancel') {
-        if (confirm('Are you sure you want to quit the game?')) {
-          playAgain = false;
-          break;
-        }
-      } else if (input.trim().toLowerCase() === 'rock' || input.trim().toLowerCase() === 'paper' || input.trim().toLowerCase() === 'scissors') {
-        playerSelection = input.trim().toLowerCase();
-      } else {
-        alert('Incorrect input! Try again');
-      }
-    } while (playerSelection === null);
-
-    if (playAgain) {
-      const computerSelection = computerPlay();
-      alert(playRound(playerSelection, computerSelection));
-      playCount++;
-    }
-
-    if(playerSelection === 'scissors') {
-
-        if(computerSelection === "paper") {
-            playerScore++;
-            return `${playerName} wins with scissors`;
-        } else if(computerSelection === 'rock') {
-            computerScore++;
-            return 'Branko wins with rock';
-        }
-    }
-
-}
-
-
-// game
-function game() {
-
     for(var i = 1; i <= 5; i++) {
        const playerSelection = playerPlay();
        const computerSelection = computerPlay();
        alert(playRound(playerSelection, computerSelection));
-
-
+      // terminal feedback.
        console.log(`${playerName}: ` + playerScore);
        console.log('Branko: ' + computerScore);
     }
 
-    let reStart = false;
-
     if(playerScore>computerScore){
-
-
-        confirm(`Match result: ${playerName} Wins: ${playerScore} - ${computerScore} . Press Ok to restart.`)
-        reset();
-        game();
+      confirm(`Match result: ${playerName} Wins: ${playerScore} - ${computerScore} . Press Ok to restart.`)
+      window.location.reload(playRound());
     }
-  } if (playerScore < computerScore) {
-    if (confirm(`Match result: Branko Wins: ${computerScore} - ${playerScore}. Press OK to restart.`)) {
-        window.location.reload('./');
+    if (playerScore < computerScore) {
+      if (confirm(`Match result: Branko Wins: ${computerScore} - ${playerScore}. Press OK to restart.`)) {
+        window.location.reload(playRound());
     }
   } else {
     if (confirm(`Match result: It is a tie! ${playerScore} - ${computerScore}. Press OK to restart.`)) {
-      window.location.reload('./');
+      window.location.reload(playRound());
     }
   }
-}
-game();
-
-       reStart = confirm(`Match result: ${playerName} Wins: ${playerScore} - ${computerScore} . Press Ok to restart.`)
-            
-    }
-    else if(playerScore<computerScore){
-       reStart = confirm(`Match result: Branko Wins: ${computerScore} - ${playerScore} . Press Ok to restart`)
-            
-    }
-    else{
-       reStart = confirm(`Match result: It is a tie! ${playerScore} - ${computerScore} . Press Ok to restart`)
-            
-    }
-    
-    if(reStart)
-     window.location.reload("./");
-
 }
